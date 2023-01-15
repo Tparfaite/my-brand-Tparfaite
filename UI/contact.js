47520a33f98e5a3e5d14aede1227853b9b13745c
Tparfaite <parfaitetwagira@gmail.com>
2023-01-15 12:52:24 +0200

    Feat:adding admin.js file -Add contact.js -Modify index.js -Modify skills.css [Finishes#]

---
+++ UI/contact.js
@@ -0,0 +1,10 @@
+const form=document.querySelector(".form");
+const email=document.getElementById("email");
+const contact=document.getElementById("contact");
+const fname=document.getElementById("name");
+const message=document.getElementById("message");
+console.log(message);
+
+form.addEventListener("submit",e=>{
+    e.preventDefault();
+})
--- UI/index.js
+++ UI/index.js
@@ -1,36 +1,100 @@
-let card=document.getElementsByClassName("card")[0];
-console.log(card);
+let card=document.getElementsByClassName("cards")[0];
+
 let cardbtn=document.querySelectorAll("cardbtn");
+
+const validContent=JSON.parse(localStorage.getItem("allBlogs"));
+   console.log(validContent);
+
+const displayContent=(validContent)=>{
+    
+console.log("content");
+   validContent.map(item=>{
+    const fullBlog=`
+    <div class="card1">
+        <div class="title">Blog title:
+            <h2>${item.blogTitle}</h2><br>
+        </div>
+        <div class="Author">Author:<h3>${item.blogAuthor}</h3></div><br>
+        
+        <div class="fullstory">
+            <p>
+            ${item.blogContent}
+               
+            </p>
+            <div class="footer">
+                <i class="fa fa-thumbs-o-up fa-2x" aria-hidden="true"></i>
+                <i class="fa fa-comment fa-2x" aria-hidden="true"></i>
+                <i class="fa fa-share-alt fa-2x" aria-hidden="true"></i>
+            </div><br>
+        </div>
+  
+    </div>
+    `
+    card.innerHTML+=fullBlog
+
+   })
+
+
+}
+
+document.addEventListener("DOMContentLoaded",(e)=>{
+    e.preventDefault();
+    displayContent(validContent);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // console.log(cardbtn);
-let card1btn=document.getElementById("card1btn");
-let card2btn=document.getElementById("card2btn");
-let card3btn=document.getElementById("card3btn");
+// let card1btn=document.getElementById("card1btn");
+// let card2btn=document.getElementById("card2btn");
+// let card3btn=document.getElementById("card3btn");
 
-let fullStory=document.getElementsByClassName("fullStory")[0];
-let full2Story=document.getElementsByClassName("full2Story")[0];
-let full3Story=document.getElementsByClassName("full3Story")[0];
-console.log(full2Story);
+// let fullStory=document.getElementsByClassName("fullStory")[0];
+// let full2Story=document.getElementsByClassName("full2Story")[0];
+// let full3Story=document.getElementsByClassName("full3Story")[0];
 
-let blog1=document.getElementsByClassName("blog1")[0];
-let blog2=document.getElementsByClassName("blog2")[0];
-let blog3=document.getElementsByClassName("blog3")[0];
+
+// let blog1=document.getElementsByClassName("blog1")[0];
+// let blog2=document.getElementsByClassName("blog2")[0];
+// let blog3=document.getElementsByClassName("blog3")[0];
 // console.log(blog2);
 
-card1btn.addEventListener("click", e=>{
-    e.preventDefault();
-    fullStory.classList.toggle("blog1Display");
-    card1btn.classList.toggle("card1bt");
-})
+// card1btn.addEventListener("click", e=>{
+//     e.preventDefault();
+//     fullStory.classList.toggle("blog1Display");
+//     card1btn.classList.toggle("card1bt");
+// })
 
 
-card2btn.addEventListener("click",e=>{
-    e.preventDefault();
-    full2Story.classList.toggle("blog1Display");
-    card2btn.classList.toggle("card1bt");
-})
+// card2btn.addEventListener("click",e=>{
+//     e.preventDefault();
+//     full2Story.classList.toggle("blog1Display");
+//     card2btn.classList.toggle("card1bt");
+// })
 
-card3btn.addEventListener("click",e=>{
-    e.preventDefault();
-    full3Story.classList.toggle("blog1Display");
-    card3btn.classList.toggle("card1bt");
-})
+// card3btn.addEventListener("click",e=>{
+//     e.preventDefault();
+//     full3Story.classList.toggle("blog1Display");
+//     card3btn.classList.toggle("card1bt");
+// })
